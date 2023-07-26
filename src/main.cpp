c161b8f4114b86576f2b4f60d68319e7f87bb470
utfeight <utfeightt@gmail.com>
2023-07-26 13:17:37 +0300

    Main: dummy test to pass github actions

--- src/main.cpp
+++ src/main.cpp
@@ -1,64 +1,44 @@
 #include <iostream>
-#include <iterator>
 #include <regex>
 #include <string>
+#include <vector>
 
 #include "reflect/regex_utils.hpp"
 
 using std::cout;
 using std::regex;
 using std::string;
+using std::vector;
 
-auto main() -> int {
-    string
-        str =  // A dummy C++ code snippet just to get transpiled into a header file
-        R"(
-        #include <iostream>
-
-        using namespace std;
-
-        // run_1()
-        // -> 1
-        // 1
-        int run_1()
-        {
-            cout << "run_1";
-            return 0;
+int main()
+{
+    // Input code containing function signatures
+    string code = R"(
+        int add(int a, int b) {
+            return a + b;
         }
 
-        // run_2()
-        // -> 2
-        // 2
-        auto run_2 = []() -> int {
-            cout << "run_2";
-            return 0;
-        };
+        double divide(double a, double b) {
+            if (b == 0.0)
+                throw std::runtime_error("Division by zero");
+            return a / b;
+        }
 
-        // run_3()
-        // -> 3
-        // 3
-        auto run_3() -> int {
-            cout << "run_3";
-            return 0;
-        };
-        )";
+        void someFunction() {
+            // Function without parameters
+        }
+    )";
 
-    // matches words beginning by "Geek"
+    // Regular expression to match function signatures
+    const regex functionSignatureRegex(R"(\b(\w+)\s+(\w+)\s*\([^)]*\))");
 
-    try {
-        using regex_utils::operator""_r;
-        regex regex_matcher = R"((\w+)\s+(\w+)\s*\((.*)\)\s*{)"_r;
-        string regex_buffer;
-        // regex_replace() for replacing the match with 'geek'
-        regex_replace(
-            back_inserter(regex_buffer),
-            str.begin(),
-            str.end(),
-            regex_matcher,
-            "<SOME FUNCTION FOUND>");
-    } catch (std::regex_error& e) {
-        // Syntax error in the regular expression
-        cout << "Error: " << e.what() << "\n";
+    // Searching for function signatures in the code
+    vector<string> fn_buf = reflect::search::find_matches(code, functionSignatureRegex);
+
+    cout << fn_buf[0] << ": first function\n";
+    for (const auto& fn : fn_buf)
+    {
+        cout << fn << "\n";
     }
 
     return 0;
